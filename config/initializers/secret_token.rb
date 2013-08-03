65299642f5c3e15a9551028dbcbde9f6e8ba4680
Casey Scarborough <caseyscarborough@gmail.com>
2013-08-03 19:24:10 -0400

    Start conversion to Rails.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+GithubActivity::Application.config.secret_key_base = 'd4f605018cba460839559320b7b188fcc50cd1d5177a23e906477ef132513b828c43d5a1dd5510d4a8cd7b20b23a1d0e0ce1482ac3d0618da99db53fdb76b76a'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+GithubActivity::Application.config.session_store :cookie_store, key: '_github-activity_session'
