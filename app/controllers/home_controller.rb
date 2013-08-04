a1163b743874abdea9ccaf8840a9088360b8c8a7
Casey Scarborough <caseyscarborough@gmail.com>
2013-08-04 16:31:02 -0400

    Remove database dependency.

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -8,8 +8,7 @@ class HomeController < ApplicationController
   end
 
   def feed
-    @username = params[:username]
-    @limit = params[:limit]
+    @username, @limit = params[:username], params[:limit]
     @user_data = JSON.parse(Net::HTTP.get_response(URI.parse("https://api.github.com/users/#{@username}")).body)
     @event_data = JSON.parse(Net::HTTP.get_response(URI.parse("https://api.github.com/users/#{@username}/events")).body)
   end
--- config/application.rb
+++ config/application.rb
@@ -1,6 +1,9 @@
 require File.expand_path('../boot', __FILE__)
 
-require 'rails/all'
+require "action_controller/railtie"
+require "action_mailer/railtie"
+require "sprockets/railtie"
+require "rails/test_unit/railtie"
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
