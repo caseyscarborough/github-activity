42f06cce4b6866579fb91025fc2e2c27c6abfcc2
Casey Scarborough <caseyscarborough@gmail.com>
2013-08-04 20:02:42 -0400

    Add fallback for user not found.

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -4,16 +4,21 @@ require 'htmlentities'
 
 class HomeController < ApplicationController
 
+  Octokit.client_id = CONFIG[:github_client_id]
+  Octokit.client_secret = CONFIG[:github_client_secret]
+
   def index
     render 'index', layout: false
   end
 
   def feed
     @username = params[:username]
-    Octokit.client_id = CONFIG[:github_client_id]
-    Octokit.client_secret = CONFIG[:github_client_secret]
-    @user = Octokit.user @username
-    @events = Octokit.user_public_events(@username)
+    begin
+      @user = Octokit.user @username
+      @events = Octokit.user_public_events(@username)
+    rescue
+      flash.now[:error] = "#{params[:username]} does not exist."
+    end
   end
 
 end
